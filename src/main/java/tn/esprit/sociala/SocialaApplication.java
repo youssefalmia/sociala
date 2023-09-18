bd0417d4fbfcf1cdf9374b95cc415d251d299789
Youssef ALMIA <almia.youssef2@gmail.com>
2023-09-18 23:43:12 +0100

    first commit

---
+++ src/main/java/tn/esprit/sociala/SocialaApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.sociala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialaApplication.class, args);
+    }
+
+}
