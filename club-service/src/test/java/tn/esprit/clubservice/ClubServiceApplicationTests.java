2714d0248da354064cee124fa9fcc427e8020cba
Youssef ALMIA <almia.youssef2@gmail.com>
2023-09-19 10:07:02 +0100

    services added

---
+++ club-service/src/test/java/tn/esprit/clubservice/ClubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.clubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
