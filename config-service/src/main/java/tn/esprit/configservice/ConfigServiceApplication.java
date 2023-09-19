0362ad9a2f48c35224059670d2f84d3000b8cd18
Youssef ALMIA <almia.youssef2@gmail.com>
2023-09-19 16:37:12 +0100

    Some config added

--- config-service/src/main/java/tn/esprit/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/tn/esprit/configservice/ConfigServiceApplication.java
@@ -2,8 +2,12 @@ package tn.esprit.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.*;
+import org.springframework.cloud.config.server.*;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
